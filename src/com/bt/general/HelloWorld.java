e2881c46fa8f7d24d06e5225e1c45a0d54ba2e43
SudheerKumarCh <41630446+SudheerKumarCh@users.noreply.github.com>
2018-11-30 18:48:03 +0530

    1st Project

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -1,15 +1,7 @@
-package com.bt.general;
-
-import java.util.Date;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to ANT !! " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to ANT !! " );
 	}
-
-	private static Date getLocalCurrentDate() {
-		return new Date();
-	}
-
 }
