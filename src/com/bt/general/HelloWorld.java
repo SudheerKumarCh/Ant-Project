4c0dab698396cb792be30431c3310c5ba14b2b7f
SudheerKumarCh <41630446+SudheerKumarCh@users.noreply.github.com>
2018-12-10 18:09:23 +0000

    New Changes
    New Changes

--- src/com/bt/general/HelloWorld.java
+++ src/com/bt/general/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to ANT project new!! " );
+		System.out.println("Hello Welcome to ANT project Chitti3.0!! " );
 	}
 }
